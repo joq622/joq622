7a2c5f3f4bc65846adb47b562dc54c0da382273c
JOQUERHCET <mserekalegoodlucky@gmail.com>
2025-04-25 14:41:08 +0300

    Update away.js

--- commands./away.js
+++ commands./away.js
@@ -1,1 +1,5 @@
-
+module.exports = 
+  name: '.away',
+  run: async (sock, msg, args) => 
+    await sock.sendMessage(msg.key.remoteJid,  text: '🤖 Iḿ not available at the moment. Iĺl get back to you soon!' );
+  ;
