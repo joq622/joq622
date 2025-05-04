85e3d2d3b6b20c9d3e015d9ef5976c8bca33fe69
JOQUERHCET <mserekalegoodlucky@gmail.com>
2025-05-04 20:19:18 +0300

    Delete Commands directory

--- Commands/Ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ping', // The name of the command
-    description: 'Replies with Pong!', // Optional description
-    async execute(sock, message, args, isGroup) {
-        const from = message.key.remoteJid;
-        await sock.sendMessage(from, { text: 'Pong!' });
-    },
-};
